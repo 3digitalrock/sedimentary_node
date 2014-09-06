a5abb25bec01ae8e3b15c9c06b815bcb9b794585
synth3tk <davidt@3digitalrockstudios.com>
2014-09-06 14:28:34 -0400

    Added 'Channel' and 'Studio' factories

--- public/js/modules/common/api_client.js
+++ public/js/modules/common/api_client.js
@@ -1,14 +1,43 @@
-angular.module('apiClient',['ngResource']).factory('Video', ['$resource', function($resource) {
-  return $resource('http://api.3drs.synth3tk.com/videos/:id', { id: '@id' }, {
-    'save': {
-      method: 'PUT'
-    },
-    'query': {
-        method:'GET',
-        isArray:false
-    },
-    'delete': {
-        method:'DELETE'
-    }
-  });
-}]);
+angular.module('apiClient',['ngResource'])
+  .factory('Video', ['$resource', function($resource) {
+    return $resource('http://api.3drs.synth3tk.com/videos/:id', { id: '@id' }, {
+      'save': {
+        method: 'PUT'
+      },
+      'query': {
+          method:'GET',
+          isArray:false
+      },
+      'delete': {
+          method:'DELETE'
+      }
+    });
+  }])
+  .factory('Channel', ['$resource', function($resource) {
+    return $resource('http://api.3drs.synth3tk.com/channels/:id', { id: '@id' }, {
+      'save': {
+        method: 'PUT'
+      },
+      'query': {
+          method:'GET',
+          isArray:false
+      },
+      'delete': {
+          method:'DELETE'
+      }
+    });
+  }])
+  .factory('Studio', ['$resource', function($resource) {
+    return $resource('http://api.3drs.synth3tk.com/studios/:id', { id: '@id' }, {
+      'save': {
+        method: 'PUT'
+      },
+      'query': {
+          method:'GET',
+          isArray:false
+      },
+      'delete': {
+          method:'DELETE'
+      }
+    });
+  }]);
