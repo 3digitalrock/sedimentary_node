3593205ce8f94433eba8b163bf2c9eaf0ae2cccf
synth3tk <davidt@3digitalrockstudios.com>
2014-09-06 17:43:06 -0400

    Add non-working modal to vidctrl

--- public/js/modules/video/video.ctrl.js
+++ public/js/modules/video/video.ctrl.js
@@ -1,28 +1,24 @@
-angular.module('videoApp', ['ngRoute', 'apiClient'])
+angular.module('videoApp', ['ngRoute', 'apiClient', 'ui.bootstrap'])
     .config(function($interpolateProvider) {
         //cfpLoadingBarProvider.includeSpinner = false;
         $interpolateProvider.startSymbol('{[{');
         $interpolateProvider.endSymbol('}]}');
     })
-    .config(['$routeProvider', '$locationProvider',
-        function($routeProvider, $locationProvider) {
-          $routeProvider
-            .when('/', {
-              templateUrl: '/views/partials/channel_home.html',
-              controller: 'ChannelHomeCtrl'
-            })
-            .when('/:videoId', {
-              templateUrl: '/views/partials/channel_details.html',
-              controller: 'ChannelDetailsCtrl'
-            })
-            .otherwise({
-              redirectTo: '/'
-            });
-    }])
-    .controller('ChannelHomeCtrl', ['$scope', '$routeParams', 'Video', function ($scope, $routeParams, Video) {
+    .controller('ChannelHomeCtrl', ['$scope', '$modal', '$routeParams', 'Video', function ($scope, $modal, $routeParams, Video) {
         Video.query().$promise.then(function(videos){
             $scope.videos = videos.items;
         }, function(errResponse) {
             // fail
         });
     }]);
+    
+var VideoModalCtrl = function ($scope, $modalInstance, videoInfo) {
+  $scope.video = videoInfo;
+  $scope.ok = function () {
+    $modalInstance.close();
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+};
