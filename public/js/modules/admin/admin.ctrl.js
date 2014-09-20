e8be9d4d03ebf5fdc8496b0a0f588c2c4ee2aa8f
synth3tk <davidt@3digitalrockstudios.com>
2014-09-19 22:30:29 -0400

    Admin reorg vid dets; Fix preview vid

--- public/js/modules/admin/admin.ctrl.js
+++ public/js/modules/admin/admin.ctrl.js
@@ -1,4 +1,4 @@
-angular.module('AdminApp', ['ngRoute', 'ui.bootstrap', 'ui.router', 'xeditable', 'restangular', 'angular-loading-bar'])
+angular.module('AdminApp', ['ngRoute', 'ui.bootstrap', 'ui.router', 'xeditable', 'restangular', 'angular-loading-bar', 'checklist-model'])
     .config(function($interpolateProvider, $locationProvider, $sceDelegateProvider, RestangularProvider, cfpLoadingBarProvider) {
         $interpolateProvider.startSymbol('{[{');
         $interpolateProvider.endSymbol('}]}');
@@ -49,35 +49,64 @@ angular.module('AdminApp', ['ngRoute', 'ui.bootstrap', 'ui.router', 'xeditable',
           $urlRouterProvider.otherwise("/dashboard");
     }])
     .controller('AdminDashboardCtrl', ['$scope', '$stateParams', 'Restangular', function ($scope, $stateParams, Restangular) {
+        $scope.contentResize
         var baseVideos = Restangular.all('videos');
         baseVideos.getList().then(function(videos){
           $scope.videos = videos;
         });
     }])
-    .controller('AdminVideoDetailsCtrl', ['$scope', '$stateParams', '$filter', 'Restangular', function ($scope, $stateParams, $filter, Restangular) {
+    .controller('AdminVideoDetailsCtrl', ['$scope', '$stateParams', '$filter', 'Restangular', '$route', '$location', '$routeParams', '$timeout',
+    function ($scope, $stateParams, $filter, Restangular, $route, $location, $routeParams, $timeout) {
         var baseVideo = Restangular.one('videos', $stateParams.videoId);
         $scope.video = baseVideo.get().$object;
-        observer = jsonpatch.observe($scope.video);
+        var observer = jsonpatch.observe($scope.video);
         
         $scope.studios = [
           {value: 'Blb5I6AEAtuj', text: '3 Digital Rock'},
           {value: 'Blb5I6AEAtuj2', text: '2 Analog Stones'}
-        ]; 
+        ];
+        
+        $scope.channels = [
+          {value: 'action', text: 'Action'},
+          {value: 'comedy', text: 'Comedy'},
+          {value: 'horror', text: 'Horror'}
+        ];
       
         $scope.showStudio = function() {
           var selected = $filter('filter')($scope.studios, {value: $scope.video.studio});
           return ($scope.video.studio && selected.length) ? selected[0].text : 'Not set';
+        };
+
+        $scope.showChannels = function() {
+          var selected = [];
+          angular.forEach($scope.channels, function(s) {
+            if ($scope.video.channels.contains(s.value)) {
+              selected.push(s.text);
+            }
+          });
+          
+          return selected.length ? selected.join(', ') : 'Not set';
         };
         
         $scope.updateVideo = function() {
           angular.copy($scope.video, $scope.videoCopy);
           var patch = jsonpatch.generate(observer);
           baseVideo.patch(patch).then(function(){
-            console.log('Okie Dokie');
+            console.log('video saved!');
           }, function(){
-            console.log('Uh-oh spaghetti-o!');
+            console.log('error saving video!');
           });
         };
+        
+        $scope.showPlayer = function(){
+          $timeout(function (){
+            GoGoPlayerRanger();
+          }, 0);
+        };
+        $scope.showPlayer();
+        $scope.$on("$destroy", function(){
+            videojs('video-player').dispose();
+        });
     }])
     .controller('AdminVideoCreateCtrl', function () {
         
@@ -120,6 +149,34 @@ angular.module('AdminApp', ['ngRoute', 'ui.bootstrap', 'ui.router', 'xeditable',
     editableOptions.theme = 'default';
   });
     
+var GoGoPlayerRanger = function () {
+  videojs('video-player', {
+    plugins: {
+      resolutions: true
+    }
+  });
+  videojs.options.flash.swf = "/js/vendor/video-js.swf";
+  videojs('video-player').ready(function(){
+    var singlePlayer = this;
+    singlePlayer.volume(0.75);
+    var aspectRatio = 9/16; // Make up an aspect ratio
+  
+    function resizeVideoJS(){
+      // Get the parent element's actual width
+      var width = document.getElementById(singlePlayer.id()).parentElement.offsetWidth;
+      // Remove the padding from the calculation
+      width = width - parseInt($("#video-player").parent().css('padding-left'));
+      width = width - parseInt($("#video-player").parent().css('padding-right'));
+      // Set width to fill parent element, Set height
+      singlePlayer.width(width).height( width * aspectRatio );
+      console.log('sized up');
+    }
+  
+    resizeVideoJS(); // Initialize the function
+    window.onresize = resizeVideoJS; // Call the function on resize
+  });
+};
+    
 var ModalInstanceCtrl = function ($scope, $modalInstance, delVideo) {
   $scope.videoInfo = delVideo;
   $scope.ok = function () {
@@ -129,4 +186,13 @@ var ModalInstanceCtrl = function ($scope, $modalInstance, delVideo) {
   $scope.cancel = function () {
     $modalInstance.dismiss('cancel');
   };
+};
+
+Array.prototype.contains = function(k) {
+  for(var i=0; i < this.length; i++){
+    if(this[i] === k){
+      return true;
+    }
+  }
+  return false;
 };
