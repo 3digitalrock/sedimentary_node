0856f771985eebf51dd20b3dce40c6b37ee8508e
synth3tk <davidt@3digitalrockstudios.com>
2014-10-09 18:09:09 -0400

    FE: Temp removed ui-routes cause fuck Firefox

--- public/js/modules/video/video.ctrl.js
+++ public/js/modules/video/video.ctrl.js
@@ -1,23 +1,11 @@
-angular.module('videoApp', ['ngRoute', 'apiClient', 'ui.bootstrap', 'ui.router', 'slick', 'angular-loading-bar', 'ngAnimate'])
+angular.module('videoApp', ['ngRoute', 'ui.router', 'slick', 'angular-loading-bar', 'ngAnimate'])
     .config(function($interpolateProvider, $locationProvider) {
         //cfpLoadingBarProvider.includeSpinner = false;
         $interpolateProvider.startSymbol('{[{');
         $interpolateProvider.endSymbol('}]}');
         $locationProvider.html5Mode(true);
     })
-    .config(['$stateProvider', '$urlRouterProvider',
-        function($stateProvider, $urlRouterProvider) {
-          $stateProvider
-            .state('channels', {
-              url: '/channels',
-              controller: 'ChannelHomeCtrl'
-            })
-            .state('watch', {
-              url: '/watch/:uid',
-              controller: 'WatchVideoCtrl'
-            });
-    }])
-    .controller('ChannelHomeCtrl', ['$scope', '$modal', '$routeParams', 'Video', '$rootScope', function ($scope, $modal, $routeParams, Video, $rootScope) {
+    .controller('ChannelHomeCtrl', ['$scope', '$routeParams', '$rootScope', function ($scope, $routeParams, $rootScope) {
       
     }])
     .controller('WatchVideoCtrl', ['$scope', '$location', function($scope, $location){
