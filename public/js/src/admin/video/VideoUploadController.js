350ca4d1d1d87bb80d2f4c5ac2cd72dd99f28bee
synth3tk <davidt@3digitalrockstudios.com>
2014-10-22 20:51:38 -0400

    Dash: Reset form on success

--- public/js/src/admin/video/VideoUploadController.js
+++ public/js/src/admin/video/VideoUploadController.js
@@ -47,18 +47,12 @@ angular.module('videoModule')
           // file is uploaded successfully
           console.log(status);
           console.log(data);
+          $scope.form = {};
         })
         .error(function(err){
           console.log(err);
         });
-        //.then(success, error, progress); 
-        // access or attach event listeners to the underlying XMLHttpRequest.
-        //.xhr(function(xhr){xhr.upload.addEventListener(...)})
       }
-      /* alternative way of uploading, send the file binary with the file's content-type.
-         Could be used to upload files to CouchDB, imgur, etc... html5 FileReader is needed. 
-         It could also be used to monitor the progress of a normal http post/put request with large data*/
-      // $scope.upload = $upload.http({...})  see 88#issuecomment-31366487 for sample code.
     };
   
   }])
