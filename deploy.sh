68caf7cd1a1727f199c34a4349565bf16b5834a8
synth3tk <dev@synth3.tk>
2014-11-05 22:22:10 -0500

    Use new tower-cli setup

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-tower-cli job launch \
-    --job-template $ANSIBLE_TOWER_JOB_TEMPLATE_ID \
-    --username $ANSIBLE_TOWER_USER  \
-    --password $ANSIBLE_TOWER_PASS  \
-    --host $ANSIBLE_TOWER_SERVER
+tower-cli config host $ANSIBLE_TOWER_SERVER && \
+tower-cli config username $ANSIBLE_TOWER_USER && \
+tower-cli config password $ANSIBLE_TOWER_PASS
+
+tower-cli job launch --job-template=$ANSIBLE_TOWER_JOB_TEMPLATE_ID
